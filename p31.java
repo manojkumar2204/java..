15fe989c0fa03ba8cc6e82f3116fb6a74cf43487
Manoj kumar Panigrahi <manojkumarpanigrahi54@gmail.com>
2025-02-19 20:31:36 +0530

    Update p31.java

--- p31.java
+++ p31.java
@@ -2,8 +2,8 @@
 
 public class p31 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = i; j < 5; j++) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = i; j < 6; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= (2 * i - 1); k++) {
